0598ff6b6454ccc8676b0f761c8b3073bbd6cd4e
Jacob McSwain <jacob@mcswain.dev>
2023-08-10 00:07:24 -0500

    manager: frontend: add some glamor

--- manager/frontend/src/views/MainPage.vue
+++ manager/frontend/src/views/MainPage.vue
@@ -3,7 +3,12 @@
     <Card>
       <template #title>Cloud Node Status</template>
       <template #content>
-        <p>**insert status info here**</p>
+        <h3>VTun Daemon</h3>
+        <p>{{ vtundRunning ? 'Running':'Stopped' }}</p>
+        <h3>OLSR Daemon</h3>
+        <p>{{ olsrdRunning ? 'Running':'Stopped' }}</p>
+        <h3>Tunnels Connected</h3>
+        <p>{{ tunnelsConnected }}/{{ totalTunnels }}</p>
       </template>
     </Card>
   </div>
@@ -12,17 +17,44 @@
 <script>
 import Card from 'primevue/card';
 
+import API from '@/services/API';
+
 export default {
   components: {
     Card,
   },
-  created() {},
+  created() {
+    this.fetchData();
+  },
   mounted() {},
   unmounted() {},
   data: function() {
-    return {};
+    return {
+      vtundRunning: true,
+      olsrdRunning: true,
+      tunnelsConnected: 0,
+      totalTunnels: 0,
+    };
   },
-  methods: {},
+  methods: {
+    fetchData() {
+      API.get('/olsr/running').then((res) => {
+        this.olsrdRunning = res.data.running;
+      });
+      API.get('/vtun/running').then((res) => {
+        this.vtundRunning = res.data.running;
+      });
+      API.get('/tunnels').then((res) => {
+        this.tunnelsConnected = 0;
+        for (const tunnel of res.data.tunnels) {
+          if (tunnel.active) {
+            this.tunnelsConnected++;
+          }
+        }
+        this.totalTunnels = res.data.total;
+      });
+    },
+  },
   computed: {},
 };
 </script>
