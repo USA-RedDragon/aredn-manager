f9e2cd2db44de27bc898d08ecd2313143401b8ec
Jacob McSwain <jacob@mcswain.dev>
2023-08-11 03:30:13 -0500

    frontend: footer: fix source link

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <p>
       This project proudly open
-      <a href="https://github.com/USA-RedDragon/aredn-virtual-node" target="_blank"
+      <a href="https://github.com/USA-RedDragon/aredn-manager" target="_blank"
         >source</a
       >
     </p>
