68b37760dab1be915fb22b6d94780b23f80ee0ea
Jacob McSwain <jacob@mcswain.dev>
2025-02-19 00:29:19 -0600

    add command to generate ipv6 addresses

---
+++ internal/utils/ips_test.go
@@ -0,0 +1,60 @@
+package utils_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/USA-RedDragon/aredn-manager/internal/utils"
+)
+
+func TestGenerateIPv6RandomAddress(t *testing.T) {
+	// Test that the generated IPs are valid
+	for i := 0; i < 1000000; i++ {
+		ip, err := utils.GenerateIPv6RandomAddress()
+		if err != nil {
+			t.Fatalf("Failed to generate randomIPv6 address: %v", err)
+		}
+		ipObj := net.ParseIP(ip)
+		if ipObj == nil {
+			t.Fatalf("Invalid IP address: %s", ip)
+		}
+		if !ipObj.IsPrivate() {
+			t.Fatalf("Non-private IP address generated: %s", ip)
+		}
+		if ipObj.IsLoopback() {
+			t.Fatalf("Loopback IP address generated: %s", ip)
+		}
+		if ipObj.IsLinkLocalUnicast() {
+			t.Fatalf("Link-local unicast IP address generated: %s", ip)
+		}
+		if ipObj.IsLinkLocalMulticast() {
+			t.Fatalf("Link-local multicast IP address generated: %s", ip)
+		}
+		if ipObj.IsInterfaceLocalMulticast() {
+			t.Fatalf("Interface-local multicast IP address generated: %s", ip)
+		}
+		if ipObj.IsMulticast() {
+			t.Fatalf("Multicast IP address generated: %s", ip)
+		}
+		if ipObj.IsUnspecified() {
+			t.Fatalf("Unspecified IP address generated: %s", ip)
+		}
+	}
+}
+
+func TestGenerateIPv6LinkLocalAddress(t *testing.T) {
+	// Test that the generated IPs are valid
+	for i := 0; i < 1000000; i++ {
+		ip, err := utils.GenerateIPv6LinkLocalAddress()
+		if err != nil {
+			t.Fatalf("Failed to generate link-local IPv6 address: %v", err)
+		}
+		ipObj := net.ParseIP(ip)
+		if ipObj == nil {
+			t.Fatalf("Invalid IP address: %s", ip)
+		}
+		if !ipObj.IsLinkLocalUnicast() {
+			t.Fatalf("Non-link-local unicast IP address generated: %s", ip)
+		}
+	}
+}
