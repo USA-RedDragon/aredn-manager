c16e6539f8bf7ab5702a240e8559ad0961314285
Jacob McSwain <jacob@mcswain.dev>
2025-02-23 13:22:04 -0600

    utils: ip: adjust how we create link-local address

--- internal/utils/ip.go
+++ internal/utils/ip.go
@@ -7,10 +7,10 @@ import (
 
 // Generate a link-local address beginning with fe80 and ending with the 4 octets of the IPv4 address
 func GenerateIPv6LinkLocalAddress(ipv4 net.IP) (string, error) {
-	ipv6 := net.ParseIP("fe80::")
+	v4Bytes := ipv4.To4()
+	ipv6 := net.ParseIP(fmt.Sprintf("fe80::%02x%02x:%02x%02x", v4Bytes[0], v4Bytes[1], v4Bytes[2], v4Bytes[3]))
 	if ipv6 == nil {
 		return "", fmt.Errorf("failed to parse IPv6 address")
 	}
-	ipv6 = append(ipv6, ipv4.To16()[12:]...)
 	return ipv6.String(), nil
 }
