5e78ea9d951c80c9d95e4e066cc90de077227322
Jacob McSwain <jacob@mcswain.dev>
2025-02-26 01:32:23 -0600

    arednlink: buffer broadcast channel

--- cmd/arednlink.go
+++ cmd/arednlink.go
@@ -31,7 +31,7 @@ func runArednlink(cmd *cobra.Command, _ []string) error {
 	routes := xsync.NewMapOf[string, string]()
 	services := xsync.NewMapOf[string, string]()
 	hosts := xsync.NewMapOf[string, string]()
-	broadcastChan := make(chan arednlink.Message)
+	broadcastChan := make(chan arednlink.Message, 1024)
 
 	arednlinkServer, err := arednlink.NewServer(config, &routes, hosts, services, broadcastChan)
 	if err != nil {
